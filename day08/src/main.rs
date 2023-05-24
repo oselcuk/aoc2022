f5c98dfad809666c79b31f406300575b5fc00d23
Oguzhan Selcuk <me@oselcuk.com>
2023-05-24 23:10:40 +0300

    Day 8
    Also add itertools to common

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,11 +1,96 @@
 use std::{env, fs};
 
+use itertools::iproduct;
+
+fn parse_input(input: String) -> Vec<Vec<i8>> {
+    input
+        .lines()
+        .map(|line| line.chars().map(|c| (c as i8) - ('0' as i8)).collect())
+        .collect()
+}
+
 fn part1(input: String) -> String {
-    String::new()
+    let map = parse_input(input);
+    let n = map.len();
+    let m = map[0].len();
+    let mut seen = vec![vec![false; m]; n];
+    for i in 0..n {
+        let mut max = -1;
+        for j in 0..m {
+            seen[i][j] = seen[i][j] || (map[i][j] > max);
+            max = max.max(map[i][j]);
+        }
+        max = -1;
+        for j in (0..m).rev() {
+            seen[i][j] = seen[i][j] || (map[i][j] > max);
+            max = max.max(map[i][j]);
+        }
+    }
+    for i in 0..m {
+        let mut max = -1;
+        for j in 0..n {
+            seen[j][i] = seen[j][i] || (map[j][i] > max);
+            max = max.max(map[j][i]);
+        }
+        max = -1;
+        for j in (0..n).rev() {
+            seen[j][i] = seen[j][i] || (map[j][i] > max);
+            max = max.max(map[j][i]);
+        }
+    }
+
+    seen.iter()
+        .map(|row| row.iter().filter(|x| **x).count())
+        .sum::<usize>()
+        .to_string()
 }
 
 fn part2(input: String) -> String {
-    String::new()
+    let map = parse_input(input);
+    let n = map.len();
+    let m = map[0].len();
+    iproduct!(1..n - 1, 1..m - 1)
+        .map(|(x, y)| {
+            let mut score = 1;
+            let z = map[x][y];
+            let mut view = 0;
+            // look up
+            for i in (0..x).rev() {
+                view += 1;
+                if map[i][y] >= z {
+                    break;
+                };
+            }
+            score *= view;
+            view = 0;
+            for i in x + 1..n {
+                view += 1;
+                if map[i][y] >= z {
+                    break;
+                };
+            }
+            score *= view;
+            view = 0;
+            for j in (0..y).rev() {
+                view += 1;
+                if map[x][j] >= z {
+                    break;
+                };
+            }
+            score *= view;
+            view = 0;
+            for j in y + 1..m {
+                view += 1;
+                if map[x][j] >= z {
+                    break;
+                };
+            }
+            score *= view;
+            score
+        })
+        .max()
+        .unwrap()
+        .to_string()
 }
 
 fn main() {

264dee2d7b3d27a20046a4835dcb9af9de213d6a
Oguzhan Selcuk <me@oselcuk.com>
2023-05-24 10:05:01 +0300

    Day 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,11 +1,90 @@
 use std::{env, fs};
 
+#[derive(PartialEq, Copy, Clone, Debug)]
+enum Move {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl TryFrom<i32> for Move {
+    type Error = ();
+
+    fn try_from(value: i32) -> Result<Self, Self::Error> {
+        let vals = [Move::Rock, Move::Paper, Move::Scissors];
+        for val in vals {
+            if value == (val as i32) {
+                return Ok(val);
+            }
+        }
+        Err(())
+    }
+}
+
+impl TryFrom<char> for Move {
+    type Error = ();
+
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        match value {
+            'A' | 'X' => Ok(Move::Rock),
+            'B' | 'Y' => Ok(Move::Paper),
+            'C' | 'Z' => Ok(Move::Scissors),
+            _ => Err(()),
+        }
+    }
+}
+
+fn parse_round_1(round: &str) -> (Move, Move) {
+    let chars: Vec<char> = round.chars().collect();
+    (chars[0].try_into().unwrap(), chars[2].try_into().unwrap())
+}
+
+fn parse_round_2(round: &str) -> (Move, Move) {
+    let chars: Vec<char> = round.chars().collect();
+    let them: Move = chars[0].try_into().unwrap();
+    let us = chars[2];
+    (
+        them,
+        match us {
+            'X' => ((them as i32 + 2) % 3).try_into().unwrap(),
+            'Y' => them,
+            'Z' => ((them as i32 + 1) % 3).try_into().unwrap(),
+            _ => panic!(),
+        },
+    )
+}
+
+fn score_round<F>(round: &str, parser: F) -> i32
+where
+    F: Fn(&str) -> (Move, Move),
+{
+    let (them, us) = parser(round);
+    // println!("Them: {them:?}\tUs: {us:?}");
+    use Move::*;
+    us as i32
+        + 1
+        + match (us, them) {
+            _ if us == them => 3,
+
+            (Rock, Paper) | (Paper, Scissors) | (Scissors, Rock) => 0,
+            _ => 6,
+        }
+}
+
 fn part1(input: String) -> String {
-    String::new()
+    input
+        .lines()
+        .map(|round| score_round(round, parse_round_1))
+        .sum::<i32>()
+        .to_string()
 }
 
 fn part2(input: String) -> String {
-    String::new()
+    input
+        .lines()
+        .map(|round| score_round(round, parse_round_2))
+        .sum::<i32>()
+        .to_string()
 }
 
 fn main() {

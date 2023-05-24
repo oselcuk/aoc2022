21edcdb15b742a3518817e86ea7124a0c9d35184
Oguzhan Selcuk <me@oselcuk.com>
2023-05-24 11:28:02 +0300

    Day 3

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,11 +1,57 @@
-use std::{env, fs};
+use std::{collections::HashSet, env, fs};
+
+fn histogram(sack: &str) -> HashSet<char> {
+    sack.chars().collect()
+}
+
+fn get_commons(sacks: &[&str]) -> char {
+    sacks
+        .into_iter()
+        .map(|sack| sack.chars().collect::<HashSet<char>>())
+        .reduce(|l, r| l.intersection(&r).cloned().collect())
+        .unwrap()
+        .into_iter()
+        .next()
+        .unwrap()
+    // sacks
+    //     .into_iter()
+    //     .map(|sack| HashSet::from_iter(sack.chars()))
+    //     .reduce(|left, right| HashSet::from_iter(left.intersection(&right).cloned().collect()));
+    // 'a'
+}
+
+fn get_common(sack: &str) -> char {
+    let (left, right) = sack.split_at(sack.len() / 2);
+    get_commons(&[left, right])
+}
+
+fn prio_code(c: char) -> i32 {
+    c as i32 + 1
+        - if c.is_lowercase() {
+            'a' as i32
+        } else {
+            'A' as i32 - 26
+        }
+}
 
 fn part1(input: String) -> String {
-    String::new()
+    input
+        .lines()
+        .map(get_common)
+        .map(prio_code)
+        .sum::<i32>()
+        .to_string()
 }
 
 fn part2(input: String) -> String {
-    String::new()
+    input
+        .lines()
+        .collect::<Vec<&str>>()
+        .chunks(3)
+        .map(get_commons)
+        .map(prio_code)
+        .sum::<i32>()
+        .to_string()
 }
 
 fn main() {
